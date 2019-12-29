256dc1fa7d1537072e4fe65fb15d2eb8610f7b96
Perman Yagmyrov <permanyagmyrov@gmail.com>
2019-12-29 02:53:49 +0200

    remade with OOP'
    :
    :

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'quiz_brain.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -26,78 +26,112 @@ class QuizPage extends StatefulWidget {
 
 
 class _QuizPageState extends State<QuizPage> {
-  List <Icon> myList = [];
-  @override
-  Widget build(BuildContext context) {
+  QuizBrain quizBrain = QuizBrain();
+  List <Icon> myList = [
+  ];
 
+void jogabym(bool userPickedAnswer){
+  bool correctAnswer = quizBrain.quizQuestionAnswer();
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-             //User picked true
-                },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        Row(
-          children: myList,
-        ),
-      ],
+  setState(() {
+
+  if(correctAnswer == userPickedAnswer){
+    myList.add(
+      Icon(
+        Icons.check,
+        color: Colors.green,
+      )
     );
   }
+  else {
+    myList.add(
+      Icon(
+        Icons.close,
+        color: Colors.red,
+      ),
+    );
+  }
+  quizBrain.nexQuestion();
+  });
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+  @override
+  Widget build(BuildContext context) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment
+                .spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment
+                .stretch,
+        children: <
+                Widget>[
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets
+                      .all(10.0),
+              child: Center(
+                child: Text(
+                  quizBrain.quizQuestionText(),
+                  textAlign: TextAlign
+                          .center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors
+                            .white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets
+                      .all(15.0),
+              child: FlatButton(
+                textColor: Colors
+                        .white,
+                color: Colors
+                        .green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors
+                            .white,
+                    fontSize: 20.0,
+                  ),
+                ),
+                onPressed: () {
+                  jogabym(true);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets
+                      .all(15.0),
+              child: FlatButton(
+                color: Colors
+                        .red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors
+                            .white,
+                  ),
+                ),
+                onPressed: () {
+                 jogabym(false);
+
+                },
+              ),
+            ),
+          ),
+          Row(
+            children: myList,
+          ),
+        ],
+      );
+    }
+}
